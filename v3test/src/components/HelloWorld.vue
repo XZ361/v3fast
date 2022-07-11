52658dbd9e4593e797066e9c376497af1312059a
XZ361 <2457307263@qq.com>
2022-07-12 07:37:36 +0800

    componsition api的使用和生命周期

---
+++ v3test/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>{{data.counter}}</p>
+  <p>{{data.doubleCounter}}</p>
+</template>
+
+<script>
+import { computed, onMounted, onUnmounted, reactive } from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup(){
+    const data = reactive({
+      counter: 0,
+      doubleCounter: computed(() => data.counter*2)
+    })
+    let timer
+    onMounted(()=>{
+      timer = setInterval(() => {
+        // data.counter++
+      }, 1000);
+    })
+    onUnmounted(()=>{
+      clearInterval(timer)
+    })
+    return {data}
+  }
+}
+</script>
