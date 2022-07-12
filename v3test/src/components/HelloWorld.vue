e57854710d2c9c1f6641dc4a77c98f445a1a05c6
XZ361 <2457307263@qq.com>
2022-07-12 08:08:48 +0800

    新的代码复用和组织逻辑

--- v3test/src/components/HelloWorld.vue
+++ v3test/src/components/HelloWorld.vue
@@ -2,11 +2,12 @@
   <h1>{{ msg }}</h1>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
   <p>{{data.counter}}</p>
+  <h3>{{msg2}}</h3>
   <p>{{data.doubleCounter}}</p>
 </template>
 
 <script>
-import { computed, onMounted, onUnmounted, reactive } from 'vue'
+import { computed, onMounted, onUnmounted, reactive, ref } from 'vue'
 
 export default {
   name: 'HelloWorld',
@@ -14,20 +15,26 @@ export default {
     msg: String
   },
   setup(){
-    const data = reactive({
-      counter: 0,
-      doubleCounter: computed(() => data.counter*2)
-    })
-    let timer
-    onMounted(()=>{
-      timer = setInterval(() => {
-        // data.counter++
-      }, 1000);
-    })
-    onUnmounted(()=>{
-      clearInterval(timer)
-    })
-    return {data}
+    const data = useCounter()
+    const msg2 = ref('hello msg2!')
+
+    return {data, msg2}
   }
+};
+function useCounter() {
+  const data = reactive({
+    counter: 0,
+    doubleCounter: computed(() => data.counter * 2)
+  })
+  let timer
+  onMounted(() => {
+    timer = setInterval(() => {
+      // data.counter++
+    }, 1000);
+  })
+  onUnmounted(() => {
+    clearInterval(timer)
+  })
+  return data
 }
 </script>
