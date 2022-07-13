47870d0d4ac2923939894c481f1e9a333232d14d
XZ361 <2457307263@qq.com>
2022-07-13 13:52:23 +0800

    custom renderer自定义渲染

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 vue3.0快速学习
 - Teleport传送门
 - Fragments和Emits选项
-- 自定义渲染器
+- 自定义渲染器 custom renderer
---
+++ v3test/src/CanvasApp.vue
@@ -0,0 +1,28 @@
+<template>
+<!-- 自定义渲染：用特殊的方式在不同的平台做特殊的事情 -->
+<!-- 画饼图：canvas、SVG；当前用canvas -->
+  <piechart @click="handleClick" :data="state.data" :x="200" :y="200" :r="200"></piechart>
+</template>
+<script>
+import { reactive, ref } from "vue";
+export default {
+
+  setup() {
+    const state = reactive({
+      data: [
+        { name: "大专", count: 200, color: "brown" },
+        { name: "本科", count: 300, color: "yellow" },
+        { name: "硕士", count: 100, color: "pink" },
+        { name: "博士", count: 50, color: "skyblue" }
+      ]
+    });
+    function handleClick() {
+      state.data.push({ name: "其他", count: 30, color: "orange" });
+    }
+    return {
+      state,
+      handleClick
+    };
+  }
+};
+</script>
