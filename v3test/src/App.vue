e97049295bdbac3e3507c3307a15febe3761f970
XZ361 <2457307263@qq.com>
2022-07-22 08:16:37 +0800

    结合composition api 写法使用vuex

--- v3test/src/App.vue
+++ v3test/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- 传统写法 -->
   <p @click="$store.commit('add')">{{$store.state.count}}</p>
+  <!-- composition api 写法 -->
+  <p @click="add">{{state.count}}</p>
   <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
   <!-- <Todos></Todos> -->
   <!-- vue-router4中keep-alive用法 -->
@@ -14,12 +17,23 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Todos from './components/todos/Todos.vue'
+import { useStore } from "vuex";
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Todos
+  },
+  setup(){
+    const store = useStore()
+    console.log(store);
+    return{
+      state: store.state,
+      add: ()=>{
+        store.commit('add')
+      }
+    }
   }
 }
 </script>
